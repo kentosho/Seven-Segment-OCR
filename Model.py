a44b4b1fcbfc69db5a168f1b8347a6555359f05f
Kento Ichikawa <kento.ichikawa@gmail.com>
2021-06-21 12:17:52 +0900

    Too many units such as 256 or 1024 reduced to 11

--- Model.py
+++ Model.py
@@ -78,7 +78,8 @@ class Model_Multi(Model):
 
         x = Flatten()(x)
 
-        x = Dense(256, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+        # x = Dense(256, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+        x = Dense(11, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
 
         digit1 = (Dense(11,activation = 'softmax', name='digit_1'))(x)
         digit2 = (Dense(11,activation = 'softmax', name='digit_2'))(x)
@@ -195,7 +196,8 @@ class Model_Single(Model):
 
         x = Flatten()(x)
 
-        x = Dense(1024, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+        # x = Dense(1024, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+        x = Dense(11, activation ='relu', kernel_regularizer=regularizers.l2(0.01))(x)
 
         output = Dense(11,activation = 'softmax', name='output')(x)
 
