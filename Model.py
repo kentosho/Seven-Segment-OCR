5939a6cd3f96ff51422dd1fb9f1f2ab54788f6c7
Kento Ichikawa <kento.ichikawa@gmail.com>
2021-06-21 16:22:10 +0900

    remove kernel_regularizer

--- Model.py
+++ Model.py
@@ -62,20 +62,20 @@ class Model_Multi(Model):
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_1')(x)
         x = Dropout(0.30)(x)
 
-        x = Conv2D(64, (3, 3), padding='same', name='conv2d_hidden_2', kernel_regularizer='l2')(x)
+        x = Conv2D(64, (3, 3), padding='same', name='conv2d_hidden_2')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_2')(x)
         x = Dropout(0.30)(x)
 
-        x = Conv2D(128, (3, 3), padding='same', name='conv2d_hidden_3', kernel_regularizer='l2')(x)
+        x = Conv2D(128, (3, 3), padding='same', name='conv2d_hidden_3')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_3')(x)
         x = Dropout(0.30)(x)
         x = Flatten()(x)
 
-        x = Dense( 256, activation ='relu', kernel_regularizer='l2')(x)
+        x = Dense( 256, activation ='relu')(x)
 
         digit1 = (Dense(11,activation = 'softmax', name='digit_1'))(x)
         digit2 = (Dense(11,activation = 'softmax', name='digit_2'))(x)
@@ -172,19 +172,19 @@ class Model_Single(Model):
 
 
         model_input = Input((100, 256, 1))
-        x = Conv2D(32, (3, 3), padding='same', name='conv2d_hidden_1', kernel_regularizer='l2')(model_input)
+        x = Conv2D(32, (3, 3), padding='same', name='conv2d_hidden_1')(model_input)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_1')(x)
         x = Dropout(0.30)(x)
 
-        x = Conv2D(63, (3, 3), padding='same', name='conv2d_hidden_2', kernel_regularizer='l2')(x)
+        x = Conv2D(63, (3, 3), padding='same', name='conv2d_hidden_2')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_2')(x)
         x = Dropout(0.30)(x)
 
-        x = Conv2D(128, (3, 3), padding='same', name='conv2d_hidden_3', kernel_regularizer='l2')(x)
+        x = Conv2D(128, (3, 3), padding='same', name='conv2d_hidden_3')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2), strides=(3, 3),name='maxpool_2d_hidden_3')(x)
@@ -192,7 +192,7 @@ class Model_Single(Model):
 
         x = Flatten()(x)
 
-        x = Dense( 1024, activation ='relu', kernel_regularizer='l2', name='dense2')(x)
+        x = Dense( 1024, activation ='relu', name='dense2')(x)
 
         output = Dense( 11,activation = 'softmax', name='output')(x)
 
